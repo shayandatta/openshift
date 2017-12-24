9edd6ad69c81e2757d6cc97cc79a016b5dd0ac88
Shayan Datta <abc199327@gmail.com>
2017-12-24 18:14:27 +0530

    done again

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import os
-from flask_openshift_template import app as application
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return 'Hello World'
 
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
     httpd.serve_forever()
+    app.run(
+        debug=True
+    )
